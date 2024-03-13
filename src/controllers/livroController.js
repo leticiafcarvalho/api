e5a199643e21d144e4de98a8b70c2664708f2045
Leticia Carvalho <leticiacarvalhos2@outlook.com>
2024-03-13 20:18:58 -0300

    crud

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -13,6 +13,26 @@ class LivroController {
         }
     }
 
+    static async listarLivroPorId (req, res) {
+      try {
+        const id = req.params.id;
+        const livroEncontrado = await livro.findById(id);
+        res.status(200).json(livroEncontrado);
+      } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha na requisição do livro` });
+      }
+    };
+
+    static async atualizarLivro (req, res) {
+      try {
+        const id = req.params.id;
+        await livro.findByIdAndUpdate(id, req.body);
+        res.status(200).json({ message: "livro atualizado" });
+      } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha na atualização` });
+      }
+    };  
+  
     static async cadastrarLivro (req, res) {
         try {
           const novoLivro = await livro.create(req.body);
@@ -20,7 +40,18 @@ class LivroController {
         } catch (erro) {
           res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` });
         }
+    };
+
+    static async excluirLivro (req, res) {
+      try {
+        const id = req.params.id;
+        await livro.findByIdAndDelete(id);
+        res.status(200).json({ message: "livro excluído com sucesso" });
+      } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha na exclusão` });
       }
+    };
+  
     
 };
 
